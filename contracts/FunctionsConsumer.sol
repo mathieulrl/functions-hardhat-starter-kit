129ea3bd140ba2f0a8f3fbe16142c8bc7e017d21
Mathieu LARUELLE <95310781+mathieulrl@users.noreply.github.com>
2023-12-10 15:43:36 +0100

    Update FunctionsConsumer.sol for enigma

--- contracts/FunctionsConsumer.sol
+++ contracts/FunctionsConsumer.sol
@@ -1,75 +1,146 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 /**
- * @title Chainlink Functions example on-demand consumer contract example
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
  */
-contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
-  using FunctionsRequest for FunctionsRequest.Request;
+contract FunctionsConsumerDenigma is FunctionsClient, ConfirmedOwner {
+    using FunctionsRequest for FunctionsRequest.Request;
+    using ECDSA for bytes32;
 
-  bytes32 public donId; // DON ID for the Functions DON to which the requests are sent
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+    string public enigma;
+    bytes32 public answer;
 
-  bytes32 public s_lastRequestId;
-  bytes public s_lastResponse;
-  bytes public s_lastError;
+    error UnexpectedRequestID(bytes32 requestId);
 
-  constructor(address router, bytes32 _donId) FunctionsClient(router) ConfirmedOwner(msg.sender) {
-    donId = _donId;
-  }
+    event Response(bytes32 indexed requestId, bytes response, bytes err);
 
-  /**
-   * @notice Set the DON ID
-   * @param newDonId New DON ID
-   */
-  function setDonId(bytes32 newDonId) external onlyOwner {
-    donId = newDonId;
-  }
+    constructor(
+        address router
+    ) FunctionsClient(router) ConfirmedOwner(msg.sender) {}
 
-  /**
-   * @notice Triggers an on-demand Functions request using remote encrypted secrets
-   * @param source JavaScript source code
-   * @param secretsLocation Location of secrets (only Location.Remote & Location.DONHosted are supported)
-   * @param encryptedSecretsReference Reference pointing to encrypted secrets
-   * @param args String arguments passed into the source code and accessible via the global variable `args`
-   * @param bytesArgs Bytes arguments passed into the source code and accessible via the global variable `bytesArgs` as hex strings
-   * @param subscriptionId Subscription ID used to pay for request (FunctionsConsumer contract address must first be added to the subscription)
-   * @param callbackGasLimit Maximum amount of gas used to call the inherited `handleOracleFulfillment` method
-   */
-  function sendRequest(
-    string calldata source,
-    FunctionsRequest.Location secretsLocation,
-    bytes calldata encryptedSecretsReference,
-    string[] calldata args,
-    bytes[] calldata bytesArgs,
-    uint64 subscriptionId,
-    uint32 callbackGasLimit
-  ) external onlyOwner {
-    FunctionsRequest.Request memory req;
-    req.initializeRequest(FunctionsRequest.Location.Inline, FunctionsRequest.CodeLanguage.JavaScript, source);
-    req.secretsLocation = secretsLocation;
-    req.encryptedSecretsReference = encryptedSecretsReference;
-    if (args.length > 0) {
-      req.setArgs(args);
+    /**
+     * @notice Send a simple request
+     * @param source JavaScript source code
+     * @param encryptedSecretsUrls Encrypted URLs where to fetch user secrets
+     * @param donHostedSecretsSlotID Don hosted secrets slotId
+     * @param donHostedSecretsVersion Don hosted secrets version
+     * @param args List of arguments accessible from within the source code
+     * @param bytesArgs Array of bytes arguments, represented as hex strings
+     * @param subscriptionId Billing ID
+     */
+    function sendRequest(
+        string memory source,
+        bytes memory encryptedSecretsUrls,
+        uint8 donHostedSecretsSlotID,
+        uint64 donHostedSecretsVersion,
+        string[] memory args,
+        bytes[] memory bytesArgs,
+        uint64 subscriptionId,
+        uint32 gasLimit,
+        bytes32 donID
+    ) external onlyOwner returns (bytes32 requestId) {
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(source);
+        if (encryptedSecretsUrls.length > 0)
+            req.addSecretsReference(encryptedSecretsUrls);
+        else if (donHostedSecretsVersion > 0) {
+            req.addDONHostedSecrets(
+                donHostedSecretsSlotID,
+                donHostedSecretsVersion
+            );
+        }
+        if (args.length > 0) req.setArgs(args);
+        if (bytesArgs.length > 0) req.setBytesArgs(bytesArgs);
+        s_lastRequestId = _sendRequest(
+            req.encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+        return s_lastRequestId;
     }
-    if (bytesArgs.length > 0) {
-      req.setBytesArgs(bytesArgs);
-    }
-    s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, callbackGasLimit, donId);
-  }
 
-  /**
-   * @notice Store latest result/error
-   * @param requestId The request ID, returned by sendRequest()
-   * @param response Aggregated response from the user code
-   * @param err Aggregated error from the user code or from the execution pipeline
-   * Either response or error parameter will be set, but never both
-   */
-  function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
-    s_lastResponse = response;
-    s_lastError = err;
-  }
+    /**
+     * @notice Send a pre-encoded CBOR request
+     * @param request CBOR-encoded request data
+     * @param subscriptionId Billing ID
+     * @param gasLimit The maximum amount of gas the request can consume
+     * @param donID ID of the job to be invoked
+     * @return requestId The ID of the sent request
+     */
+    function sendRequestCBOR(
+        bytes memory request,
+        uint64 subscriptionId,
+        uint32 gasLimit,
+        bytes32 donID
+    ) external onlyOwner returns (bytes32 requestId) {
+        s_lastRequestId = _sendRequest(
+            request,
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+        return s_lastRequestId;
+    }
+
+    /**
+     * @notice Store latest result/error
+     * @param requestId The request ID, returned by sendRequest()
+     * @param response Aggregated response from the user code
+     * @param err Aggregated error from the user code or from the execution pipeline
+     * Either response or error parameter will be set, but never both
+     */
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        if (s_lastRequestId != requestId) {
+            revert UnexpectedRequestID(requestId);
+        }
+        s_lastResponse = response;
+        s_lastError = err;
+        emit Response(requestId, s_lastResponse, s_lastError);
+    }
+
+
+    /**
+     * @notice Set the enigma value
+     * @param _enigma The enigma value to set
+     */
+    function setEnigma(string memory _enigma) external onlyOwner {
+        enigma = _enigma;
+    }
+
+
+   /**
+     * @notice Set the answer value
+     * @param rawAnswer The raw answer value toset
+     */
+    function setAnswer(string memory rawAnswer) external onlyOwner {
+        answer = keccak256(bytes(rawAnswer));
+    }
+    
+    /**
+     * @notice Verify if a hash provided as a parameter is equal to the stored answer hash
+     * @param hash The hash to verify
+     * @return true if the hash is equal to the stored answer hash, false otherwise
+     */
+    function verifyHash(bytes32 hash) external view returns (bool) {
+        return hash == answer;
+    }
+
+
+
 }
